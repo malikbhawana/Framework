b3d266b34661d5d442a3cdfeec8d82311c137e28
VE0011255 <VE0011255@FA006738.YMI.COM>
2020-04-11 18:14:42 +0530

    New change

--- Framework/src/test/java/com/pymidol/pages/BaseClass.java
+++ Framework/src/test/java/com/pymidol/pages/BaseClass.java
@@ -10,7 +10,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Parameters;
+//import org.testng.annotations.Parameters;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
